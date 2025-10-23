72eaab93831e3bf2985fadbbbcdb2bf735ce8f08
OticasVisiotech <gabrielmgb.nd@gmail.com>
2025-10-22 23:38:57 -0300

    Organized code from Mission

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -27,19 +27,19 @@ const Mission = () => {
             <source src={mission} type="video/mp4" />
           </motion.video>
           <motion.div
-           initial={{ opacity: 0 }}
-           whileInView={{ opacity: 1 }}
-           viewport={{ once: true }}
-           transition={{ duration: 0.5, delay: 0.5 }}
-
-          className="absolute h-full w-full rounded-3xl bg-black/40"></motion.div>
-          <motion.p 
-           initial={{ opacity: 0, y: 20 }}
-           whileInView={{ opacity: 1, y: 0 }}
-           viewport={{ once: true }}
-           transition={{ duration: 1, delay: 0.5 }}
-
-          className="absolute max-w-lg tracking-tighter lg:text-3xl">
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className="absolute h-full w-full rounded-3xl bg-black/40"
+          ></motion.div>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="absolute max-w-lg tracking-tighter lg:text-3xl"
+          >
             {MISSION}
           </motion.p>
         </div>
